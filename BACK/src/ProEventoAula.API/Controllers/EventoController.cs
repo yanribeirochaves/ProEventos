4b9a3824824119ed5d5a9407f78db670af485c6b
Yan <yanribeirochaves@gmail.com>
2022-03-30 17:46:24 -0300

    projeto completo e projeto de estudo

---
+++ BACK/src/ProEventoAula.API/Controllers/EventoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventoAula.API.Data;
+using ProEventoAula.API.Models;
+
+namespace ProEventoAula.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
+        {
+            this._context = context;
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+           return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+        }
+        
+        [HttpPost]
+        public string Post()
+        {
+            return "post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return "put" + id;
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "delete" + id;
+        }
+    }
+}
---
+++ BACK/src/ProEventoAula.API/Models/Evento.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventoAula.API.Models
+{
+    public class Evento
+    {
+        public int EventoId {get; set; }
+        public string Local {get; set; }
+        public string DataEvento {get; set; }
+        public string Tema {get; set; }
+        public string Lote {get; set; }
+        public int QtdPessoa {get; set; }
+        public string ImagemURL {get; set; }
+    }
+}
